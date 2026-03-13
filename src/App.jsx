1f7652a1f63e19c1871b127205bbb02b18ce3007
Muco Abu <mbekere1@outlook.com>
2026-03-12 17:43:22 -0700

    Add coming-soon flag and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,34 @@
-import './App.css'
-import BuiltForEveryoneSection from './components/BuiltForEveryoneSection'
-import CapabilitiesSection from './components/CapabilitiesSection'
-import Footer from './components/Footer'
-import HeroSection from './components/HeroSection'
-import KeepingGuardianFreeSection from './components/KeepingGuardianFreeSection'
-import TeamSection from './components/TeamSection'
-import WhyGuardianMattersSection from './components/WhyGuardianMattersSection'
+import "./App.css";
+import BuiltForEveryoneSection from "./components/BuiltForEveryoneSection";
+import CapabilitiesSection from "./components/CapabilitiesSection";
+import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
+import KeepingGuardianFreeSection from "./components/KeepingGuardianFreeSection";
+import TeamSection from "./components/TeamSection";
+import WhyGuardianMattersSection from "./components/WhyGuardianMattersSection";
 
 function App() {
+  const isComingSoon = import.meta.env.VITE_COMING_SOON === "true";
+
   return (
     <main className="page-shell">
       <HeroSection />
-      <WhyGuardianMattersSection />
-      <BuiltForEveryoneSection />
-      <CapabilitiesSection />
-      <KeepingGuardianFreeSection />
-      <TeamSection />
-      <Footer />
+      {isComingSoon ? (
+        <div className="coming-soon">
+          <p>🚧 Site under development – coming soon!</p>
+        </div>
+      ) : (
+        <>
+          <WhyGuardianMattersSection />
+          <BuiltForEveryoneSection />
+          <CapabilitiesSection />
+          <KeepingGuardianFreeSection />
+          <TeamSection />
+          <Footer />
+        </>
+      )}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
